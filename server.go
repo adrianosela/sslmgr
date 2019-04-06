ffd075c356738b3654721f25dc4f89c2189d058d
Adriano <adriano.selaviles@gmail.com>
2019-04-06 02:36:15 -0700

    add tests

--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"strings"
 	"syscall"
 	"time"
@@ -24,27 +25,58 @@ type SecureServer struct {
 	serveSSLFunc               func() bool
 	httpsPort                  string
 	httpPort                   string
-	readTimeout                time.Duration
-	writeTimeout               time.Duration
-	idleTimeout                time.Duration
 	gracefulnessTimeout        time.Duration
 	gracefulShutdownErrHandler func(error)
 }
 
-// ServerConfig is the configuration type for a SecureServer
+// ServerConfig holds configuration to initialize a SecureServer.
+// Requied Fields: Hostnames and Handler
+// Note that it is strongly recommended not to use the default CertCache
 type ServerConfig struct {
-	// Required Fields
+	// Hostnames for which the server is allowed to serve HTTPS.
+	// If the server receives an https request through a DNS name or IP
+	// not contained in this list, the request will be denied
+	// (REQUIRED)
 	Hostnames []string
-	Handler   http.Handler
-	// Optional Fields
-	ServeSSLFunc               func() bool
-	CertCache                  autocert.Cache
-	HTTPSPort                  string
-	HTTPPort                   string
-	ReadTimeout                time.Duration
-	WriteTimeout               time.Duration
-	IdleTimeout                time.Duration
-	GracefulnessTimeout        time.Duration
+
+	// The server's http handler
+	// (REQUIRED)
+	Handler http.Handler
+
+	// ServeSSLFunc is called to determine whether to serve HTTPS
+	// or not. This function's enables users to purpusely disable
+	// HTTPS i.e. for local development.
+	// Default behavior is to serve HTTPS
+	ServeSSLFunc func() bool
+
+	// An implementation of the autocert.Cache interface, which autocert
+	// will use to store and manage certificates. It is strongly recommended
+	// to provide this field.
+	// Default behavior is to store at "." in the file system
+	CertCache autocert.Cache
+
+	// Default value is ":443"
+	HTTPSPort string
+
+	// Default value is ":80"
+	HTTPPort string
+
+	// Default value is 5 seconds
+	ReadTimeout time.Duration
+
+	// Default value is 5 seconds
+	WriteTimeout time.Duration
+
+	// Default value is 25 seconds
+	IdleTimeout time.Duration
+
+	// Default value is 5 seconds
+	GracefulnessTimeout time.Duration
+
+	// GracefulShutdownErrHandler is called to handle the event of an error during
+	// error during a gracefull shutdown (accept no more connections, and wait
+	// for existing ones to finish within the GracefulnessTimeout)
+	// Default value is a NOP
 	GracefulShutdownErrHandler func(error)
 }
 
@@ -52,15 +84,20 @@ var (
 	// ErrNoHostname is returned whenever a user calls NewSecureServer
 	// without any hostnames in the config
 	ErrNoHostname = errors.New("no hostnames provided")
+
 	// ErrNoHandler is returned whenever a user calls NewSecureServer
 	// with a nil http.Handler in the config
 	ErrNoHandler = errors.New("server handler cannot be nil")
+
+	// ErrNotAnInteger is returned whenever a user calls NewSecureServer with
+	// port definitions which do not correspont to integers. i.e. "not a number"
+	ErrNotAnInteger = errors.New("port number must be a numerical string")
 )
 
 // NewSecureServer initializes a new secure server
 func NewSecureServer(c ServerConfig) (*SecureServer, error) {
 	// check required fields
-	if len(c.Hostnames) < 1 {
+	if c.Hostnames == nil || len(c.Hostnames) < 1 {
 		return nil, ErrNoHostname
 	}
 	if c.Handler == nil {
@@ -70,15 +107,21 @@ func NewSecureServer(c ServerConfig) (*SecureServer, error) {
 	if c.CertCache == nil {
 		c.CertCache = autocert.DirCache(".")
 	}
-	// port definitions cant be empty
+	// port definitions cant be empty or non numerical strings
 	if c.HTTPSPort == "" {
 		c.HTTPSPort = ":443"
+	}
+	if _, err := strconv.Atoi(strings.TrimPrefix(c.HTTPSPort, ":")); err != nil {
+		return nil, ErrNotAnInteger
 	}
 	if !strings.HasPrefix(c.HTTPSPort, ":") {
 		c.HTTPSPort = fmt.Sprintf(":%s", c.HTTPSPort)
 	}
 	if c.HTTPPort == "" {
 		c.HTTPPort = ":80"
+	}
+	if _, err := strconv.Atoi(strings.TrimPrefix(c.HTTPPort, ":")); err != nil {
+		return nil, ErrNotAnInteger
 	}
 	if !strings.HasPrefix(c.HTTPPort, ":") {
 		c.HTTPPort = fmt.Sprintf(":%s", c.HTTPPort)
@@ -119,9 +162,11 @@ func NewSecureServer(c ServerConfig) (*SecureServer, error) {
 			HostPolicy: autocert.HostWhitelist(c.Hostnames...),
 			Cache:      c.CertCache,
 		},
-		serveSSLFunc: c.ServeSSLFunc,
-		httpPort:     c.HTTPPort,
-		httpsPort:    c.HTTPSPort,
+		serveSSLFunc:               c.ServeSSLFunc,
+		httpPort:                   c.HTTPPort,
+		httpsPort:                  c.HTTPSPort,
+		gracefulnessTimeout:        c.GracefulnessTimeout,
+		gracefulShutdownErrHandler: c.GracefulShutdownErrHandler,
 	}, nil
 }
 
---
+++ server_test.go
@@ -0,0 +1,96 @@
+package sslmgr
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+	"time"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestSecureServer(t *testing.T) {
+	Convey("Test NewSecureServer()", t, func() {
+		Convey("Test Required Field - Hostnames nil", func() {
+			ss, err := NewSecureServer(ServerConfig{
+				Handler: http.NotFoundHandler(),
+			})
+			So(ss, ShouldBeNil)
+			So(err, ShouldEqual, ErrNoHostname)
+		})
+		Convey("Test Required Field - Hostnames empty", func() {
+			ss, err := NewSecureServer(ServerConfig{
+				Handler:   http.NotFoundHandler(),
+				Hostnames: []string{},
+			})
+			So(ss, ShouldBeNil)
+			So(err, ShouldEqual, ErrNoHostname)
+		})
+		Convey("Test Required Field - Handler", func() {
+			ss, err := NewSecureServer(ServerConfig{
+				Hostnames: []string{"yourdomain.io"},
+			})
+			So(ss, ShouldBeNil)
+			So(err, ShouldEqual, ErrNoHandler)
+		})
+		Convey("Test Required Fields Suffice", func() {
+			ss, err := NewSecureServer(ServerConfig{
+				Handler:   http.NotFoundHandler(),
+				Hostnames: []string{"yourdomain.io"},
+			})
+			So(ss, ShouldNotBeNil)
+			So(err, ShouldBeNil)
+		})
+		Convey("Test Default Values Are Applied", func() {
+			ss, err := NewSecureServer(ServerConfig{
+				Handler:   http.NotFoundHandler(),
+				Hostnames: []string{"yourdomain.io"},
+			})
+			So(err, ShouldBeNil)
+			So(ss, ShouldNotBeNil)
+			So(ss.certMgr, ShouldNotBeNil)
+			So(ss.serveSSLFunc, ShouldNotBeNil)
+			So(ss.serveSSLFunc(), ShouldEqual, true)
+			So(ss.httpPort, ShouldEqual, ":80")
+			So(ss.httpsPort, ShouldEqual, ":443")
+			So(ss.server.ReadTimeout, ShouldEqual, 5*time.Second)
+			So(ss.server.IdleTimeout, ShouldEqual, 25*time.Second)
+			So(ss.server.WriteTimeout, ShouldEqual, 5*time.Second)
+			So(ss.gracefulnessTimeout, ShouldEqual, 5*time.Second)
+			So(ss.gracefulShutdownErrHandler, ShouldNotBeNil)
+			So(func() {
+				ss.gracefulShutdownErrHandler(errors.New("Hello World"))
+			}, ShouldNotPanic)
+		})
+		Convey("Test Port Address Correction", func() {
+			ss, err := NewSecureServer(ServerConfig{
+				Handler:   http.NotFoundHandler(),
+				Hostnames: []string{"yourdomain.io"},
+				HTTPPort:  "80",
+				HTTPSPort: "443",
+			})
+			So(err, ShouldBeNil)
+			So(ss, ShouldNotBeNil)
+			So(ss.httpPort, ShouldEqual, ":80")
+			So(ss.httpsPort, ShouldEqual, ":443")
+		})
+		Convey("Test HTTP Port Address Failure", func() {
+			ss, err := NewSecureServer(ServerConfig{
+				Handler:   http.NotFoundHandler(),
+				Hostnames: []string{"yourdomain.io"},
+				HTTPPort:  "not an int",
+			})
+			So(ss, ShouldBeNil)
+			So(err, ShouldEqual, ErrNotAnInteger)
+		})
+		Convey("Test HTTPS Port Address Failure", func() {
+			ss, err := NewSecureServer(ServerConfig{
+				Handler:   http.NotFoundHandler(),
+				Hostnames: []string{"yourdomain.io"},
+				HTTPSPort: "not an int",
+			})
+			So(ss, ShouldBeNil)
+			So(err, ShouldEqual, ErrNotAnInteger)
+		})
+	})
+}
